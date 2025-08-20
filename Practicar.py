1e3c9852aeb9465fd20beb4ae1f27bb2bc87c023
Erwin202581 <erickboy69@gmail.com>
2025-08-19 22:10:02 -0500

    Delete Practicar.py

--- Practicar.py
+++
@@ -1,5 +0,0 @@
-"""Suma de dos números"""
-num1 = int(input("Escriba el primer numero: "))
-num2 = int(input("Escriba el segundo número: "))
-resultado = num1 + num2
-print("La suma de dos números es de :", resultado)
