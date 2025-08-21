f9a9e23ac4cbdbdf91f0592f0bfc6d6241ac6878
Erwin202581 <erickboy69@gmail.com>
2025-08-21 08:31:53 -0500

    Add files via upload

---
+++ conversion_grados.py
@@ -0,0 +1,6 @@
+""" Conversión de grados: Convertir grados Celsius a Fahrenheit"""
+print("Ingrese los grados a convertir:")
+grados_celsious = float(input(""))
+GradosF = (grados_celsious * 9/5) + 32
+print(grados_celsious, "grados celsious, equivale a: ",
+      GradosF, "grados Fahrenheit")
