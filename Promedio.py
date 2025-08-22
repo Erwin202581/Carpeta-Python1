8c3591dfb386fea471d05be3fa7fcdeca7a51c85
Erwin202581 <erickboy69@gmail.com>
2025-08-21 20:26:07 -0500

    Add files via upload

---
+++ Promedio.py
@@ -0,0 +1,11 @@
+"""Promedio de tres notas: Calcular el promedio de tres notas ingresadas por
+el usuario
+"""
+print("Ingresa la primera nota")
+Nota1 = float(input())
+print("Ingrese la segunda nota")
+Nota2 = float(input())
+print("Ingrese la tercera nota")
+Nota3 = float(input())
+Promedio = (Nota1 + Nota2 + Nota3) / 3
+print("El promedio de las notas es de: ", Promedio)
